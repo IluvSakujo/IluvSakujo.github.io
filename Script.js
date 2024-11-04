218d86a817098e7652a965fec9cd0d4fc3252ffa
IluvSakujo <Demetruis.jr@icloud.com>
2024-11-04 14:56:15 -0500

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,42 @@
+function showit(var1){
+document.getElementById("divvy1").style.display='none'
+document.getElementById("divvy2").style.display='none'
+document.getElementById("divvy3").style.display='none'
+document.getElementById("divvy4").style.display='none'
+document.getElementById(var1).style.display='block'
+}
+
+function changeme() {
+
+var element = document.getElementById("div1")
+  
+  if (element.style.fontSize=== "100px"){
+  element.style.fontSize= "90px";
+  element.innerText = "DJ"
+  }
+  else if (element.style.fontSize=== "90px"){
+  element.style.fontSize= "80px";
+  }
+  else if (element.style.fontSize=== "80px"){
+  element.style.fontSize= "70px";
+  }
+  else if (element.style.fontSize=== "70px"){
+  element.style.fontSize= "60px";
+  }
+  else if (element.style.fontSize=== "60px"){
+  element.style.fontSize= "50px"
+  }
+  else if (element.style.fontSize=== "50px"){
+  element.style.fontSize= "40px"
+  }
+  else if (element.style.fontSize=== "40px"){
+  element.style.fontSize= "30px"}
+  else if (element.style.fontSize=== "30px"){
+  element.style.fontSize= "20px"}
+  else if (element.style.fontSize=== "20px"){
+  element.style.fontSize= "10px"
+  }
+  else if (element.style.fontSize=== "10px"){
+  element.style.fontSize= "100px"
+  }
+  }

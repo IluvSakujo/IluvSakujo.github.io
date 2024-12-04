1cb3566bc6edcfd66015bf1648a43b36eb900b12
IluvSakujo <Demetruis.jr@icloud.com>
2024-12-04 13:24:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function showit(var1) {
+  document.getElementById("divvy1").style.display = "none"
+  document.getElementById("divvy2").style.display = "none"
+  document.getElementById("divvy3").style.display = "none"
+  document.getElementById("divvy4").style.display = "none"
+  document.getElementById("divvy5").style.display = "none"
+  document.getElementById(var1).style.display = "block"
+}
 
